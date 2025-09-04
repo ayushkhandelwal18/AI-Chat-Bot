9db4c815ea7212db2c2b55f1a14ca82cd8935da5
ayush.404 <149388383+ayushkhandelwal18@users.noreply.github.com>
2025-09-04 18:38:16 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 // Middleware 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-chat-bot-cauj.onrender.com/",
     credentials:true,
 }));
 app.use(express.json());
